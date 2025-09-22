44a0c545ec3709d864c38cab62d645ffdc3da3d6
saketh <srsaketh1901@gmail.com>
2025-09-23 03:56:24 +0530

    removed policy functionality

--- mcp_server.py
+++ mcp_server.py
@@ -2,17 +2,15 @@ from mcp.server.fastmcp import FastMCP
 from tools.leave_tool import register_leave_tool
 from tools.send_email_tool import register_send_email_tool
 from tools.linkedin_tool import register_linkedin_tool
-from tools.employee_tool import register_employee_tool  # <-- Add this import
-
+from tools.employee_tool import register_employee_tool  
 
 
 mcp = FastMCP("HR Agent")
 register_leave_tool(mcp)
 register_send_email_tool(mcp)
 register_linkedin_tool(mcp)
-register_employee_tool(mcp) 
+register_employee_tool(mcp)
+# register_policy_tool(mcp) 
 
 if __name__ == "__main__":
-    # Remove prints to avoid breaking MCP handshake for Claude
-    # print("🟢 HR Agent MCP Server is starting...")
-    mcp.run()  # Use default port 3333, no arguments
+    mcp.run()
